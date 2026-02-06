734c096fffd326164d169310ed04f7d6d45926a2
RamGoate <dmturben@hotmail.com>
2026-02-06 14:05:21 -0600

    player animation

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -14,3 +14,41 @@ func _physics_process(_delta):
 	# Move and Slide function uses velocity of character body to move character on map
 	move_and_slide()
 	#move_and_collide()
+
+#animation
+var left = false
+
+
+
+func _ready() -> void:
+	$animation.play("idlefront")
+
+func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("right"):
+		$animation.play("walkside")
+		left = false
+	
+	if Input.is_action_just_released("right"):
+		$animation.play("idleside")
+	
+	if velocity.x:
+		$animation.flip_h = velocity.x < 0
+	
+	if Input.is_action_just_pressed("left"):
+		$animation.play("walkside")
+		
+	if Input.is_action_just_released("left"):
+		$animation.play("idleside")
+		
+	if Input.is_action_just_pressed("up"):
+		$animation.play("walkback")
+		
+	if Input.is_action_just_released("up"):
+		$animation.play("idleback")
+		
+	if Input.is_action_just_pressed("down"):
+		$animation.play("walkfront")
+		
+	if Input.is_action_just_released("down"):
+		$animation.play("idlefront")
