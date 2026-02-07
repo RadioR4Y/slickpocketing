0a85a1fece0ec51dafa45d8dab641f64edf957eb
RadioRay <rustelk2930@gmail.com>
2026-02-07 16:49:33 -0600

    Target interact label when player enters area

--- Assets/Scripts/target_a.gd
+++ Assets/Scripts/target_a.gd
@@ -3,3 +3,12 @@ extends Node2D
 
 func _ready() -> void:
 	$animation.play("idlefront")
+	$InteractSprite.hide()
+
+func _on_target_hit_area_entered(area: Area2D):
+	if area.is_in_group("hitbox"):
+		$InteractSprite.show()
+
+func _on_target_hit_area_exited(area: Area2D):
+	if area.is_in_group("hitbox"):
+		$InteractSprite.hide()
